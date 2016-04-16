fc6e47e603fd2d063975aca0f944d00f882cd1b7
LegaTux <josmpena91@gmail.com>
2016-04-16 12:37:24 +0200

    Create class for each piece type

---
+++ bishop.rb
@@ -0,0 +1,6 @@
+class Bishop < Piece
+  def move_valid?(new_position)
+    @position.char[0] == new_position.char[0] ||
+      @position.char[1] == new_position.char[1];
+  end
+end
---
+++ knight.rb
@@ -0,0 +1,6 @@
+class Knight < Piece
+  def move_valid?(new_position)
+    @position.char[0] == new_position.char[0] ||
+      @position.char[1] == new_position.char[1];
+  end
+end
